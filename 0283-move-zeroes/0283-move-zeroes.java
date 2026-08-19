404e0078b9a4db85c746e0aef9bc1272b7432636
Aditya Rajput <adityarajput.182009@gmail.com>
2026-08-19 19:47:22 +0530

    Time: 3 ms (18.1%), Space: 47.8 MB (57.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,8 +2,9 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int scan = 0;
         int insert = 0;
+        int n = nums.length;
 
-        while(scan<nums.length && insert<nums.length){
+        while(scan<n && insert<n){
             if(nums[scan]!=0){
                 int temp = nums[insert];
                 nums[insert] = nums[scan];
