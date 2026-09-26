83b2c17e447d9bae883d9fc146857bb15250b3f3
Aditya Rajput <adityarajput.182009@gmail.com>
2026-09-26 20:54:15 +0530

    Time: 1 ms (24.44%), Space: 45.1 MB (40.17%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,44 +1,37 @@
 class Solution {
     public int findGCD(int[] nums) {
-        //smallest number
-    int small = nums[0];
-    for(int i=0;i<nums.length; i++){
-        if(nums[i]<small){
-            small = nums[i];
+        int max = nums[0];
+        int min = nums[0];
+        // finding max and min
+        for (int i = 1; i<nums.length; i++){
+            if (nums[i]>max){
+                max = nums[i];
+            }
+            if (nums[i]<min){
+                min = nums[i];
+            }
         }
-    }
-    //largestnumber
-    int large = nums[0];
-    for(int i=0;i<nums.length; i++){
-        if(nums[i]>large){
-            large = nums[i];
+        ArrayList<Integer> minFactors = new ArrayList<>();
+        for (int i = 1; i<=Math.sqrt(min);i++){
+            if(min%i==0){
+                minFactors.add(i);
+                if(!minFactors.contains(min/i)){
+                    minFactors.add(min/i);
+                }
+            }
         }
-    }
-    //find divisiors
-    //small
-    List<Integer> smalldivisors = new ArrayList<>();
-    for(int i = 1; i<=small; i++){
-        if(small%i == 0){
-            smalldivisors.add(i);
+        int temp = 1;
+        for(int i =1; i<=Math.sqrt(max); i++){
+            if(max%i==0){
+                if(minFactors.contains(i) && i>temp){
+                    temp = i;
+                    
+                }
+                if(minFactors.contains(max/i) && max/i>temp){
+                        temp = max/i;
+                    }
+            }
         }
-    }
-    //large
-    List<Integer> largedivisors = new ArrayList<>();
-    for(int i = 1; i<=large; i++){
-        if(large%i == 0){
-            largedivisors.add(i);
-        }
-    }
-    //sorting
-    Collections.sort(smalldivisors);
-    Collections.sort(largedivisors);
-
-    // find gcd
-    for(int i = smalldivisors.size()-1; i>=0; i--){
-        if(largedivisors.contains(smalldivisors.get(i))){
-            return smalldivisors.get(i);
-        }
-    }
-    return 0;
+        return temp;
     }
 }
